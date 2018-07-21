ba9c3243bc18a0c7be4ec3cd7e5703f1bf06fdd3
James Chong <wei3hua2@gmail.com>
2018-07-21 08:44:18 +0800

    update actions

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,39 @@ import {RpsContext,RpsModule,rpsAction} from 'rpscript-interface';
 @RpsModule("cheerio")
 export default class RPSCheerio {
 
+  @rpsAction({verbName:'load-cheerio'})
+  async load (ctx:RpsContext,opts:Object, ...params:any[]) : Promise<CheerioStatic|Cheerio>{
+    if(params.length === 1)
+      return cheerio.load(params[0]);
+    else if(params.length === 2)
+      return cheerio(params[0],params[1]);
+    else if(params.length === 3)
+      return cheerio(params[0],params[1],params[2]);
+
+    else throw new Error('Invalid number of arguments');
+  }
+
   @rpsAction({verbName:'cheerio'})
-  async load (ctx:RpsContext,opts:Object, htmlContent:any) : Promise<CheerioStatic>{
-    return cheerio.load(htmlContent);
+  async cheerio (ctx:RpsContext,opts:Object, obj:any) : Promise<Cheerio>{
+    return cheerio(obj);
   }
 
   @rpsAction({verbName:'query-cheerio'})
-  async execute (ctx:RpsContext,opts:Object, obj:any, ...chain:any[]) : Promise<string>{
+  async execute (ctx:RpsContext,opts:Object, obj:Cheerio, ...chain:any[]) : Promise<any>{
 
     //map data structure
-    let placeholder:any = [{fn:'',param:[]}];
+    // let placeholder:any = [{fn:'',param:[]}];
+    let placeholder:any = [];
     chain.forEach(c => {
       if(typeof c === 'symbol')placeholder.push({fn:c,param:[]});
       else placeholder[ placeholder.length -1 ].param.push(c);
     });
 
     //execute
-    let output = obj.apply(this,placeholder[0].param);
+    // let output = obj.apply(this,placeholder[0].param);
+    let output:any = obj;
 
-    for(var i=1;i<placeholder.length;i++){
+    for(var i=0;i<placeholder.length;i++){
       let item = placeholder[i];
       let fnName = item.fn.toString();
 
--- test/index.ts
+++ test/index.ts
@@ -9,14 +9,14 @@ m.describe('Cheerio', () => {
   m.it('should load content', async function () {
     let c = new RPSCheerio;
 
-    let output = await c.load(new RpsContext,{},'<h2 class="title">Hello <span class="wei">wei</span> world</h2>');
+    let output:any = await c.load(new RpsContext,{},'h2','<h2 class="title">Hello <span class="wei">wei</span> world</h2>');
     
-    expect(output('.wei').text()).to.be.equals('wei');
+    expect(output.text()).to.be.equals('Hello wei world');
 
-    let chain = await c.execute(new RpsContext,{},output,'.wei', Symbol('text'));
-    let chain2 = await c.execute(new RpsContext,{},output,'h2', Symbol('children'),Symbol('first'),Symbol('text'));
+    let chain = await c.execute(new RpsContext,{},output, Symbol('text'));
+    let chain2 = await c.execute(new RpsContext,{},output,Symbol('children'),Symbol('first'),Symbol('text'));
 
-    expect(chain).to.be.equals('wei');
+    expect(chain).to.be.equals('Hello wei world');
     expect(chain2).to.be.equals('wei');
 
   }).timeout(0);
